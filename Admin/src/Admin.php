ad197ca37f5d463ad555e58fa61ee1632aaec2ea
Louis Charette <charette.louis@gmail.com>
2020-06-27 20:34:37 -0400

    Added Account & Admin to prototype

---
+++ Admin/src/Admin.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace UserFrosting\Demo;
+
+use UserFrosting\System\UserFrosting;
+
+class App
+{
+    public static function main(): void
+    {
+        $uf = new UserFrosting();
+        ///$uf->registerSprinkle(...);
+    }
+}
